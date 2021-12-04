bd0400efad3326eef377702852b65cd28f8be610
Mateusz <m.jagienski@gmail.com>
2021-12-04 13:07:15 +0100

    test

--- src/stopklatka/MainApp.java
+++ src/stopklatka/MainApp.java
@@ -16,24 +16,28 @@ public class MainApp extends Application {
             Parent root = FXMLLoader.load(getClass().getResource(pathToFXML));
 
             Scene scene = new Scene(root, 1350, 850);
-            this.theStage.setTitle(pageTitle);
-            this.theStage.setScene(scene);
-            this.theStage.show();
+            theStage.setTitle(pageTitle);
+            theStage.setScene(scene);
+            theStage.show();
         } catch (Exception e) {
             System.out.println("Couldn't load page " + pathToFXML);
             e.printStackTrace();
         }
     }
 
+    public Stage getTheStage() {
+        return theStage;
+    }
+
 
     @Override
     public void start(Stage stage) throws Exception {
-        this.theStage = stage;
+        theStage = stage;
 
         Parent root = FXMLLoader.load(getClass().getResource("/stopklatka/log_view/LoginPage.fxml"));
         Scene scene = new Scene(root,1350, 850);  //SETS A NEW SCENE WITH THE LOADED FXML AND DEFINES THE WDITH/HEIGHT
-        this.theStage.setScene(scene);   //SETS THE SCENE TO THE STAGE
-        this.theStage.show();
+        theStage.setScene(scene);   //SETS THE SCENE TO THE STAGE
+        theStage.show();
     }
 
 
--- src/stopklatka/log_controller/SignUpPageController.java
+++ src/stopklatka/log_controller/SignUpPageController.java
@@ -1,4 +1,134 @@
 package stopklatka.log_controller;
 
-public class SignUpPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import stopklatka.MainApp;
+import stopklatka.database.DatabaseConnection;
+import stopklatka.model.User;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SignUpPageController implements Initializable {
+
+    @FXML
+    private Button signupSubmitButton, signupCancelButton;
+    @FXML
+    private TextField signupFirstNameField, signupLastNameField, signupEmailField, signupPhoneFiled,
+            signupUsernameField;
+    @FXML
+    private PasswordField signupPasswordField, signupConfirmPasswordField;
+
+    private MainApp mainApp;
+
+    private Connection connection;
+    private DatabaseConnection databaseConnection;
+    private PreparedStatement pst;
+
+    @FXML
+    public void setSignupCancelButton(MouseEvent mouseEvent) {
+        Stage stage = (Stage) signupCancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void setSignupSubmitButton(MouseEvent mouseEvent) {
+        if (validateUser()) {
+            System.out.println("Gituwa");
+            User newUser = new User(signupFirstNameField.getText(), signupLastNameField.getText(),
+                            signupEmailField.getText(), signupPhoneFiled.getText(),
+                            signupUsernameField.getText(), signupPasswordField.getText(), "Customer");
+
+
+            String insertUser = "INSERT INTO `projekt_stopklatka`.`user`\n" +
+                    "(`first_name`,\n" +
+                    "`last_name`,\n" +
+                    "`email`,\n" +
+                    "`phone_number`,\n" +
+                    "`role`,\n" +
+                    "`password`,\n" +
+                    "`username`)\n" +
+                    "VALUES (?,?,?,?,?,?,?)";
+
+            System.out.println(insertUser);
+
+            connection = databaseConnection.getConnection();
+            try {
+                pst = connection.prepareStatement(insertUser);
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+
+            try {
+                pst.setString(1, signupFirstNameField.getText());
+                pst.setString(2, signupLastNameField.getText());
+                pst.setString(3, signupEmailField.getText());
+                pst.setString(4, signupPhoneFiled.getText());
+                pst.setString(5, "Customer");
+                pst.setString(6, signupPasswordField.getText());
+                pst.setString(7, signupUsernameField.getText());
+
+                pst.executeUpdate();
+
+
+            } catch (SQLException e1) {
+
+                e1.printStackTrace();
+            }
+
+
+        } else {
+            System.out.println("nie gituwa");
+        }
+    }
+
+    @FXML
+    public boolean validateUser() {
+        Pattern name = Pattern.compile("^[a-zA-Z]+$", Pattern.CASE_INSENSITIVE);
+        Pattern password = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$");
+        //password must contain 1 number (0-9)
+//        password must contain 1 uppercase letters
+//        password must contain 1 lowercase letters
+//        password must contain 1 non-alpha numeric number
+//        password is 8-16 characters with no space
+        Pattern phoneNumber = Pattern.compile("^\\d{9}$");
+        Pattern email = Pattern.compile(".+@.+\\..+");
+        Pattern username = Pattern.compile("\\w+");
+
+
+        System.out.println(name.matcher(signupFirstNameField.getText()).find() + " " +
+                name.matcher(signupLastNameField.getText()).find() + " " +
+                password.matcher(signupPasswordField.getText()).find() + " " +
+                phoneNumber.matcher(signupPhoneFiled.getText()).find() + " " +
+                email.matcher(signupEmailField.getText()).find() + " " +
+                username.matcher(signupUsernameField.getText()).find());
+
+        return name.matcher(signupFirstNameField.getText()).find() &&
+                name.matcher(signupLastNameField.getText()).find() &&
+                password.matcher(signupPasswordField.getText()).find() &&
+                phoneNumber.matcher(signupPhoneFiled.getText()).find() &&
+                email.matcher(signupEmailField.getText()).find() &&
+                username.matcher(signupUsernameField.getText()).find() &&
+                password.matcher(signupConfirmPasswordField.getText()).find() &&
+                signupPasswordField.getText().equals(signupConfirmPasswordField.getText());
+
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        databaseConnection = new DatabaseConnection();
+    }
+
+    public static void main(String[] args) {
+    }
 }
